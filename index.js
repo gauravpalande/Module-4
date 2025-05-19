2edb3b49e1856f827dabc592a0705674dd1243f9
Gaura Palande <Gaurav.Palande147@gmail.com>
2025-05-19 12:42:09 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const yargs = require('yargs');
+const { add, subtract } = require('./calculator');
+
+const options = yargs(process.argv);
+
+console.log("Hello World");
+console.log(add(5, 10));
+console.log(subtract(10, 5));
